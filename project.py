d95df42d64828a1fc3931254128d07567df8cbf2
jackbolton901 <bltnjck@gmail.com>
2019-08-30 14:29:46 -0500

    weekly

---
+++ project.py
@@ -0,0 +1,42 @@
+# problem 1
+
+# born = int(input("What year were you born? "))
+# def ageCalc(bornYear):
+#     age = 2019 - bornYear
+#     return age
+# print(ageCalc(born))
+
+# problem 2
+# userName = input("enter a name: ")
+# if userName == "Kenn" or "Kevin" or "Erin" or "Autumn":
+#     print("Hello teacher!!!")
+# else:
+#     print("Hello student!!!")
+
+
+# problem 3
+# userNum = int(input("Enter a negative number: "))
+# for n in range(7, userNum - 1, -1):
+#     print(n)
+
+# problem 4
+
+# yourNum = 0
+# while yourNum > -5 or yourNum < -10:
+#     yourNum = int(input("Enter a number that is between -5 and -10: "))
+# print("Good job!")
+
+# problem 5
+
+# squad = ["one", "two", "three", "four", "five"]
+# for n in squad:
+#     print(n)
+# problem 6
+
+def fullName(firstName):
+    lastName = input("What is your last name? ")
+    name = f"Hello {firstName} {lastName}"
+    print(name)
+fullName("Jack")
+
+problem 7
